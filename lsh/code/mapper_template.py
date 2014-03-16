9164cafd0558dfb010fa4009808aacc1d9227293
dagrun <dagrunh@gmail.com>
2014-03-16 14:32:32 +0100

    added for loops, that iterates over however many hashfunctions we want.

--- lsh/code/mapper_template.py
+++ lsh/code/mapper_template.py
@@ -5,19 +5,20 @@ import sys
 
 
 def partition(video_id, shingles):
-    minShing = ((a[0]*shingles[0])+b[0])%1000
-    for shingle in shingles:
-        hashShingle = ((a[0]*shingle)+b[0])%1000
-        if hashSingle < minShing:
-            minShing = hashShingle
-    sigmatrix[0,video_id] = minShing
+    for i in range(0,2):
+        minShing = ((a[i]*shingles[0])+b[i])%1000
+        for shingle in shingles:
+            hashShingle = ((a[i]*shingle)+b[i])%1000
+            if hashSingle < minShing:
+                minShing = hashShingle
+        sigmatrix[i,video_id] = minShing
 
 if __name__ == "__main__":
     # Very important. Make sure that each machine is using the
     # same seed when generating random numbers for the hash functions.
     np.random.seed(seed=42)
-    a = np.random.randint(1,1000,20)
-    b = np.random.randint(0,1000,20)
+    a = np.random.randint(1,1000,2)
+    b = np.random.randint(0,1000,2)
     sigmatrix = np.array([], ndmin=2)
     
     for line in sys.stdin:
@@ -25,3 +26,4 @@ if __name__ == "__main__":
         video_id = int(line[6:15])
         shingles = np.fromstring(line[16:], sep=" ")
         partition(video_id, shingles)
+
