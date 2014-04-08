8ef5caeee2d74691e96cf16ed48ea7a17bfee185
Álvaro Marco <amarco90@gmail.com>
2014-04-08 13:10:56 -0700

    read input file

--- svm/code/mapper.py
+++ svm/code/mapper.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python2.7
 
+import sys
+import numpy as np
+from sklearn import svm
+
 # This function has to either stay in this form or implement the
 # feature mapping. For details refer to the handout pdf.
 def transform(x_original):
     return x_original
 
 if __name__ == "__main__":
-    pass
+    x = np.zeros(shape=(0, 400))
+    y = []
+    for line in sys.stdin:
+        all_data = np.fromstring(line, sep=" ")
+        
+        y.append(all_data.item(0))
+        x = np.vstack( (x, all_data[1:]) )
+        
+
