7b5625d60dade3346d945f3f3d837c9cd6978545
Álvaro Marco <amarco90@gmail.com>
2014-04-10 09:45:39 -0700

    Training svm

--- svm/code/mapper.py
+++ svm/code/mapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import sys
+import uuid
 import numpy as np
 from sklearn import svm
 
@@ -9,13 +10,23 @@ from sklearn import svm
 def transform(x_original):
     return x_original
 
+def emit(key, value):
+    print('%s\t%s' % (key, value))
+
 if __name__ == "__main__":
-    x = np.zeros(shape=(0, 400))
+    X = np.zeros(shape=(0, 400))
     y = []
     for line in sys.stdin:
         all_data = np.fromstring(line, sep=" ")
         
         y.append(all_data.item(0))
-        x = np.vstack( (x, all_data[1:]) )
-        
+        X = np.vstack( (X, all_data[1:]) )
+    
+    y = np.array(y)
+    
+    clf = svm.SVC(kernel='linear', gamma=0.001, C=100., verbose=True, max_iter=2)
+    clf.fit(X, y)
+#    print clf.coef_
+    
+#    emit(uuid.uuid4(), )
 
