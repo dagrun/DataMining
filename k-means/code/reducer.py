a231d85de42daceae200070ea59089399ffdd85e
Álvaro Marco <amarco90@gmail.com>
2014-05-09 17:18:10 +0200

    mean with repeated elements

--- k-means/code/reducer.py
+++ k-means/code/reducer.py
@@ -12,30 +12,18 @@ if __name__ == "__main__":
         line = line.strip()
         key, value = line.split('\t')
         sample = literal_eval(value)
-        result = sample
         
-#        if XA is None:
-#            XA = np.asarray(sample)
-#        else:
-#            XB = np.asarray(sample)
-#            distances = cdist(XA, XB, 'euclidean')
-#            
-#            max_value = 10000
-#            result = []
-#            while(distances.min() < max_value):
-#                distances_as_list = list(np.array(distances).reshape(-1,))
-#                mins = sorted([x for x in distances_as_list if x < max_value])
-#                
-#                for x in xrange(distances.shape[0]):
-#                    for y in xrange(distances.shape[1]):
-#                    
-#                        if distances[x][y] == mins[0]:
-#                            result.append((XA[x] + XB[y])/float(2))
-#                            distances[x,:] = max_value
-#                            distances[:,y] = max_value
+        if XA is None:
+            XA = np.asarray(sample)
+        else:
+            XB = np.asarray(sample)
+            distances = cdist(XA, XB, 'euclidean').tolist()
+            
+            for i in xrange(len(distances)):
+                XA[i] = (XA[i] + XB[distances[i].index(min(distances[i]))]) / float(2)
     
     i = 0
-    centers = result
+    centers = XA
     while i < len(centers):
         j = 0
         while j < len(centers[i]):
