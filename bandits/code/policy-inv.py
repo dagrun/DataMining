109e5437beb773993bb74171b025b89a0c86c41d
Álvaro Marco <amarco90@gmail.com>
2014-06-08 17:20:08 +0200

    final version

--- bandits/code/policy-inv.py
+++ bandits/code/policy-inv.py
@@ -9,6 +9,7 @@ x_t = -1 # recommended article in this round
 z_t = [] # user features in this round 6x1 vector
 
 article_features = {}
+M_x = {}
 M_x_inv = {}
 b_x = {}
 
@@ -20,18 +21,19 @@ def set_articles(art):
 # This function will be called by the evaluator.
 # Check task description for details.
 def update(reward):
+    global M_x
     global M_x_inv
     global b_x
     
     if reward >= 0:
-        cur = M_x_inv[x_t]
-        
-        M_x_inv[x_t] -= (cur * np.dot(z_t, z_t.T) * cur)/(1 + z_t.T * cur * z_t)
+        M_x[x_t] += np.dot(z_t, z_t.T)
+        M_x_inv[x_t] = np.linalg.inv(M_x[x_t])
         b_x[x_t] += reward * z_t
 
 # This function will be called by the evaluator.
 # Check task description for details.
 def reccomend(timestamp, user_features, articles):
+    global M_x
     global M_x_inv
     global b_x
     global x_t
@@ -43,9 +45,10 @@ def reccomend(timestamp, user_features, articles):
     z_tT = z_t.T
     
     for art in articles:
-        if art not in M_x_inv:
-            M_x_inv[art] = np.identity(d)
+        if art not in M_x:
+            M_x[art] = np.identity(d)
             b_x[art] = np.zeros((d, 1))
+            M_x_inv[art] = np.copy(M_x[art])
 
         M_x_inverse = M_x_inv[art]
 
